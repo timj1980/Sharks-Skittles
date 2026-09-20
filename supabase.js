e453def2b3314c01e95c648765a44c29ab0992a8
timj1980 <timj1980@gmail.com>
2026-09-20 13:57:15 +0100

    initial upload

---
+++ supabase.js
@@ -0,0 +1,7 @@
+// supabase.js
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://crhafmhiiamznmdonhtu.supabase.co",
+  "sb_publishable_4vX0R7aw4USGWf-wAuyv2g_f440aaQ-"
+);
